8a7a9a9c61e36cba3af6523ec3e581d7e53b5428
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2020-02-29 23:46:45 +0200

    Make non-block server

--- index.js
+++ index.js
@@ -10,28 +10,55 @@ const pool = new Pool({
 });
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 const HOST = '0.0.0.0';
-
-let WAIT = process.env.WAIT || 1000;
-WAIT = Number(WAIT);
+const WAIT = Number(process.env.WAIT || 1000);
+const COLLECT_METRIC_PERIOD = process.env.COLLECT_METRIC_PERIOD || 20000;
+let processedCount = 0;
 
 app.use(express.static('public'));
 
+let buffer = [];
+
 app.post('/buy', (req, res) => {
-    const first = Date.now();
-    while (Date.now() - first < WAIT) {
-        Math.pow(Math.random(), Math.random());
-    }
-    pool.query('INSERT INTO metric(id, created_at) VALUES (DEFAULT, DEFAULT)', (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.status(200).json(results.rows);
-    })
+    buffer.push({
+        req,
+        res,
+        createdAt: Date.now(),
+        remove: false,
+        wait: (buffer.length + 1) * WAIT,
+    });
 });
+
+setInterval(() => {
+    buffer.forEach((message) => {
+        if (Date.now() - message.createdAt > message.wait) {
+            message
+                .res
+                .status(200)
+                .send();
+            message.remove = true;
+            processedCount++;
+        }
+    });
+    buffer = buffer.filter(value => !value.remove)
+}, 20);
+
+setInterval(() => {
+    pool.query(`
+        INSERT INTO buy_metric(count, created_at, collect_metric_group) 
+        VALUES (${processedCount}, DEFAULT, ${Date.now() / COLLECT_METRIC_PERIOD})
+        ON CONFLICT (collect_metric_group) DO UPDATE SET 
+                                  count = EXCLUDED.count + buy_metric.count,
+                                  created_at = EXCLUDED.created_at,
+                                  collect_metric_group = EXCLUDED.collect_metric_group
+                                  `,
+        (results, error) => {
+        console.log(results, error);
+        console.log(`${new Date().toISOString()} :: processed ${processedCount}`);
+    });
+    processedCount = 0;
+}, COLLECT_METRIC_PERIOD);
 
 app.listen(3000, HOST, () => {
     console.log(`Application hosted on: http://${HOST}:${PORT}`);
--- init.sql
+++ init.sql
@@ -1,4 +1,16 @@
 CREATE TABLE metric (
   id SERIAL NOT NULL PRIMARY KEY,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
+
+create table buy_metric
+(
+  count integer default 0 not null,
+  created_at timestamp default now() not null,
+  collect_metric_group bigint default 0 not null
+);
+
+alter table buy_metric owner to postgres;
+
+create unique index buy_metric_collect_metric_group_uindex
+  on buy_metric (collect_metric_group);
